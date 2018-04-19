86ca0b772a553780d968b839fc914ffa41590e12
YOGESH GOSAVI <ybgosavi12@gmail.com>
2018-04-19 12:51:45 +0530

    Nav Drawer ,Tabbed Activitiy ,fragment, material UI

--- materialsample/src/main/java/com/codekul/materialsample/Main2Activity.kt
+++ materialsample/src/main/java/com/codekul/materialsample/Main2Activity.kt
@@ -2,11 +2,22 @@ package com.codekul.materialsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        setSupportActionBar(toolbar)
+        // enabling action bar app icon and behaving it as toggle button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
 }
